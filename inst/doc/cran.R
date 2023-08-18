5e146a86d0ea260f9a788efef4702db6fa05f41c
Clement Lee <clement.lee.tm@outlook.com>
2023-08-18 12:42:34 +0000

    version 0.3.2

--- inst/doc/cran.R
+++ inst/doc/cran.R
@@ -21,6 +21,11 @@ df0.cran <- get_dep_all_packages()
 head(df0.cran)
 dplyr::count(df0.cran, type, reverse) # numbers in general larger than above
 
+## ---- echo = FALSE------------------------------------------------------------
+df1.cran <- df0.cran |> dplyr::count(from, type, reverse) |> dplyr::count(from)
+v9.all <- dplyr::filter(df1.cran, n == 9L)$from
+v0.all <- dplyr::filter(df1.cran, n == 10L)$from
+
 ## ----get_graph_all_packages---------------------------------------------------
 g0.depends <- get_graph_all_packages(type = "depends")
 g0.depends
@@ -31,16 +36,16 @@ g0.depends
 #  g0.rev_depends
 
 ## ----forward_equivalent-------------------------------------------------------
-g1.depends <- df0.cran %>%
-    dplyr::filter(type == "depends" & !reverse) %>%
-    df_to_graph(nodelist = dplyr::rename(df0.cran, name = from))
+g1.depends <- df0.cran |>
+  dplyr::filter(type == "depends" & !reverse) |>
+  df_to_graph(nodelist = dplyr::rename(df0.cran, name = from))
 g1.depends # same as g0.depends
 
 ## ----reverse_equivalent, eval = FALSE-----------------------------------------
 #  # Not run
-#  g1.rev_depends <- df0.cran %>%
-#      dplyr::filter(type == "depends" & reverse) %>%
-#      df_to_graph(nodelist = dplyr::rename(df0.cran, name = from))
+#  g1.rev_depends <- df0.cran |>
+#    dplyr::filter(type == "depends" & reverse) |>
+#    df_to_graph(nodelist = dplyr::rename(df0.cran, name = from))
 #  g1.rev_depends # should be same as g0.rev_depends
 
 ## ----is_dag-------------------------------------------------------------------
@@ -48,26 +53,26 @@ igraph::is_dag(g0.depends)
 igraph::is_dag(g1.depends)
 
 ## ----external_rev_defunct-----------------------------------------------------
-df1.rev_depends <- df0.cran %>%
-    dplyr::filter(type == "depends" & reverse) %>%
-    df_to_graph(nodelist = NULL, gc = FALSE) %>%
-    igraph::as_data_frame() # to obtain the edge list
-df1.depends <- df0.cran %>%
-    dplyr::filter(type == "depends" & !reverse) %>%
-    df_to_graph(nodelist = NULL, gc = FALSE) %>%
-    igraph::as_data_frame()
+df1.rev_depends <- df0.cran |>
+  dplyr::filter(type == "depends" & reverse) |>
+  df_to_graph(nodelist = NULL, gc = FALSE) |>
+  igraph::as_data_frame() # to obtain the edge list
+df1.depends <- df0.cran |>
+  dplyr::filter(type == "depends" & !reverse) |>
+  df_to_graph(nodelist = NULL, gc = FALSE) |>
+  igraph::as_data_frame()
 dfa.diff.depends <- dplyr::anti_join(
-    df1.rev_depends,
-    df1.depends,
-    c("from" = "to", "to" = "from")
+  df1.rev_depends,
+  df1.depends,
+  c("from" = "to", "to" = "from")
 )
 head(dfa.diff.depends)
 
 ## ----external_rev_defunct_again-----------------------------------------------
 dfb.diff.depends <- dplyr::anti_join(
-    df1.depends,
-    df1.rev_depends,
-    c("from" = "to", "to" = "from")
+  df1.depends,
+  df1.rev_depends,
+  c("from" = "to", "to" = "from")
 )
 head(dfb.diff.depends)
 
@@ -76,38 +81,38 @@ df0.summary <- dplyr::count(df0.cran, from, type, reverse)
 head(df0.summary)
 
 ## ----tops---------------------------------------------------------------------
-df0.summary %>%
-    dplyr::filter(reverse) %>%
-    dplyr::group_by(type) %>%
-    dplyr::top_n(1, n)
+df0.summary |>
+  dplyr::filter(reverse) |>
+  dplyr::group_by(type) |>
+  dplyr::top_n(1, n)
 
 ## ----summary_plot, out.width="660px", out.height="660px", fig.width=9, fig.height=9----
-df1.summary <- df0.summary %>%
-    dplyr::count(type, reverse, n)
-gg0.summary <- df1.summary %>%
-    dplyr::mutate(reverse = ifelse(reverse, "reverse", "forward")) %>%
-    ggplot2::ggplot() +
-    ggplot2::geom_point(ggplot2::aes(n, nn)) +
-    ggplot2::facet_grid(type ~ reverse) +
-    ggplot2::scale_x_log10() +
-    ggplot2::scale_y_log10() +
-    ggplot2::labs(x = "Degree", y = "Number of packages") +
-    ggplot2::theme_bw(20)
+df1.summary <- df0.summary |>
+  dplyr::count(type, reverse, n)
+gg0.summary <- df1.summary |>
+  dplyr::mutate(reverse = ifelse(reverse, "reverse", "forward")) |>
+  ggplot2::ggplot() +
+  ggplot2::geom_point(ggplot2::aes(n, nn)) +
+  ggplot2::facet_grid(type ~ reverse) +
+  ggplot2::scale_x_log10() +
+  ggplot2::scale_y_log10() +
+  ggplot2::labs(x = "Degree", y = "Number of packages") +
+  ggplot2::theme_bw(20)
 gg0.summary
 
 ## ----visualise----------------------------------------------------------------
 prefix <- "http://CRAN.R-project.org/package=" # canonical form
 degrees <- igraph::degree(g0.depends)
-df0.nodes <- data.frame(id = names(degrees), value = degrees) %>%
-    dplyr::mutate(title = paste0('<a href=\"', prefix, id, '\">', id, '</a>'))
+df0.nodes <- data.frame(id = names(degrees), value = degrees) |>
+  dplyr::mutate(title = paste0('<a href=\"', prefix, id, '\">', id, '</a>'))
 df0.edges <- igraph::as_data_frame(g0.depends, what = "edges")
 
 ## ----visNetwork---------------------------------------------------------------
 set.seed(2345L)
-vis0 <- visNetwork::visNetwork(df0.nodes, df0.edges, width = "100%", height = "720px") %>%
-    visNetwork::visOptions(highlightNearest = TRUE) %>%
-	visNetwork::visEdges(arrows = "to", color = list(opacity = 0.5)) %>%
-	visNetwork::visNodes(fixed = TRUE) %>%
-	visNetwork::visIgraphLayout(layout = "layout_with_drl")
+vis0 <- visNetwork::visNetwork(df0.nodes, df0.edges, width = "100%", height = "720px") |>
+  visNetwork::visOptions(highlightNearest = TRUE) |>
+  visNetwork::visEdges(arrows = "to", color = list(opacity = 0.5)) |>
+  visNetwork::visNodes(fixed = TRUE) |>
+  visNetwork::visIgraphLayout(layout = "layout_with_drl")
 vis0
 
--- inst/doc/degree.R
+++ inst/doc/degree.R
@@ -12,18 +12,18 @@ library(ggplot2)
 
 ## -----------------------------------------------------------------------------
 g0.imports <- get_graph_all_packages(type = "imports")
-d0.imports <- g0.imports %>% igraph::degree(mode = "in")
+d0.imports <- g0.imports |> igraph::degree(mode = "in")
 df0.imports <-
-    data.frame(name = names(d0.imports), degree = as.integer(d0.imports)) %>%
-    dplyr::arrange(dplyr::desc(degree), name)
+  data.frame(name = names(d0.imports), degree = as.integer(d0.imports)) |>
+  dplyr::arrange(dplyr::desc(degree), name)
 head(df0.imports, 10)
 
 ## -----------------------------------------------------------------------------
 g0.rev_imports <- get_graph_all_packages(type = "reverse imports")
-d0.rev_imports <- g0.rev_imports %>% igraph::degree(mode = "out") # note the difference to above
+d0.rev_imports <- g0.rev_imports |> igraph::degree(mode = "out") # note the difference to above
 df0.rev_imports <-
-    data.frame(name = names(d0.rev_imports), degree = as.integer(d0.rev_imports)) %>%
-    dplyr::arrange(dplyr::desc(degree), name)
+  data.frame(name = names(d0.rev_imports), degree = as.integer(d0.rev_imports)) |>
+  dplyr::arrange(dplyr::desc(degree), name)
 head(df0.rev_imports, 10)
 
 ## -----------------------------------------------------------------------------
@@ -32,99 +32,95 @@ setdiff(df0.imports, df0.rev_imports)
 setdiff(df0.rev_imports, df0.imports)
 
 ## -----------------------------------------------------------------------------
-df1.imports <- df0.imports %>%
-	dplyr::filter(degree > 0L) %>% # to prevent warning when plotting on log-log scale
-    dplyr::count(degree, name = "frequency") %>%
-	dplyr::arrange(dplyr::desc(degree)) %>%
-	dplyr::mutate(survival = cumsum(frequency)/sum(frequency))
+df1.imports <- df0.imports |>
+  dplyr::filter(degree > 0L) |> # to prevent warning when plotting on log-log scale
+  dplyr::count(degree, name = "frequency") |>
+  dplyr::arrange(degree) |>
+  dplyr::mutate(survival = 1.0 - cumsum(frequency) / sum(frequency))
 
 ## -----------------------------------------------------------------------------
-gg0 <- df1.imports %>%
-	ggplot2::ggplot() +
-	ggplot2::geom_point(aes(degree, frequency), size = 0.75) +
-	ggplot2::scale_x_log10() +
-	ggplot2::scale_y_log10() +
-	ggplot2::coord_cartesian(ylim = c(1L, 1e+3L)) +
-	ggplot2::theme_bw(12)
+plot_log_log <- function(df) {
+  ## useful function for below
+  df |>
+    ggplot2::ggplot() +
+    ggplot2::scale_x_log10() +
+    ggplot2::scale_y_log10() +
+    ggplot2::theme_bw(12)
+}
+gg0 <- df1.imports |>
+  plot_log_log() +
+  ggplot2::geom_point(aes(degree, frequency), size = 0.75) +
+  ggplot2::coord_cartesian(ylim = c(1L, 2e+3L))
 gg0
 
 ## ----results = FALSE----------------------------------------------------------
-x <- dplyr::filter(df0.imports, degree > 0L)$degree # data
-u <- 1L # threshold
-xi1 <- 1.0 # initial value
-a_xi1 <- 0.0 # lower bound of uniform distribution
-b_xi1 <- 100.0 # upper bound of uniform distribution
+x <- df1.imports$degree
+counts <- df1.imports$frequency
+alpha0 <- 1.5 # initial value
+theta0 <- 1.0 # initial value, but fixed to 1.0 for discrete power law
+m_alpha <- 0.0 # prior mean
+s_alpha <- 10.0 # prior standard deviation
 set.seed(3075L)
-mcmc0.imports <- mcmc_upp(x = x, u = u, xi1 = xi1, a_xi1 = a_xi1, b_xi1 = b_xi1) # takes seconds
+mcmc0.imports <-
+  mcmc_pol(
+    x = x,
+    count = counts,
+    alpha = alpha0,
+    theta = theta0,
+    a_alpha = m_alpha,
+    b_alpha = s_alpha,
+    a_theta = 1.0,
+    b_theta = 1.0,
+    a_pseudo = 10.0,
+    b_pseudo = 1.0,
+    pr_power = 1.0,
+    iter = 2500L,
+    thin = 1L,
+    burn = 500L,
+    freq = 500L,
+    invt = 1.0
+  ) # takes seconds
 
 ## -----------------------------------------------------------------------------
-mcmc0.imports %>%
-    ggplot2::ggplot() +
-	ggplot2::geom_density(aes(xi1)) +
-	ggplot2::theme_bw(12)
-
-## -----------------------------------------------------------------------------
-mcmc0.imports <- mcmc0.imports %>%
-    dplyr::mutate(alpha = 1.0 / xi1 + 1.0)
-mcmc0.imports %>%
-	ggplot2::ggplot() +
-	ggplot2::geom_density(aes(alpha)) +
-	ggplot2::theme_bw(12)
+mcmc0.imports$pars |>
+  ggplot2::ggplot() +
+  ggplot2::geom_density(aes(alpha)) +
+  ggplot2::theme_bw(12)
 
 ## -----------------------------------------------------------------------------
 n0 <- sum(df1.imports$frequency) # TOTAL number of data points in x
 ## or n0 <- length(x)
-n1 <- length(df1.imports$frequency) # number of UNIQUE data points
-N <- length(mcmc0.imports$xi1)
-freq0 <- surv0 <- matrix(as.numeric(NA), N, n1)
-for (i in seq(N)) {
-    freq0[i,] <- n0 * dupp(x = df1.imports$degree, u = 1L, xi1 = mcmc0.imports$xi1[i])
-    surv0[i,] <- Supp(x = df1.imports$degree, u = 1L, xi1 = mcmc0.imports$xi1[i])
-}
-df1.imports <- df1.imports %>%
-    dplyr::mutate(
-	    frequency.mean = apply(freq0, 2, mean),
-		frequency.qlow = apply(freq0, 2, quantile, p = 0.025),
-		frequency.qupp = apply(freq0, 2, quantile, p = 0.975),
-		survival.mean = apply(surv0, 2, mean),
-		survival.qlow = apply(surv0, 2, quantile, p = 0.025),
-		survival.qupp = apply(surv0, 2, quantile, p = 0.975)
-	)
-
-## -----------------------------------------------------------------------------
-gg1 <- df1.imports %>%
-    ggplot2::ggplot() +
-	ggplot2::geom_point(aes(degree, frequency), size = 0.75) +
-	ggplot2::geom_line(aes(degree, frequency.mean), col = 4, lty = 2) +
-	ggplot2::geom_line(aes(degree, frequency.qlow), col = 2, lty = 3) +
-	ggplot2::geom_line(aes(degree, frequency.qupp), col = 2, lty = 3) +
-	ggplot2::scale_x_log10() +
-	ggplot2::scale_y_log10() +
-	ggplot2::coord_cartesian(ylim = c(1L, 1e+3L)) +
-	ggplot2::theme_bw(12)
+df0.fitted <- mcmc0.imports$fitted
+gg1 <- df1.imports |>
+  plot_log_log() +
+  ggplot2::geom_point(aes(degree, frequency), size = 0.75) +
+  ggplot2::geom_line(aes(x, f_med * n0), data = df0.fitted, col = 4, lty = 2) +
+  ggplot2::geom_line(aes(x, f_025 * n0), data = df0.fitted, col = 2, lty = 3) +
+  ggplot2::geom_line(aes(x, f_975 * n0), data = df0.fitted, col = 2, lty = 3) +
+  ggplot2::coord_cartesian(ylim = c(1L, 2e+3L))
 gg1
 
 ## -----------------------------------------------------------------------------
-gg2 <- df1.imports %>%
-    ggplot2::ggplot() +
-	ggplot2::geom_point(aes(degree, survival), size = 0.75) +
-	ggplot2::geom_line(aes(degree, survival.mean), col = 4, lty = 2) +
-	ggplot2::geom_line(aes(degree, survival.qlow), col = 2, lty = 3) +
-	ggplot2::geom_line(aes(degree, survival.qupp), col = 2, lty = 3) +
-	ggplot2::scale_x_log10() +
-	ggplot2::scale_y_log10() +
-	ggplot2::theme_bw(12)
+gg2 <- df1.imports |>
+  plot_log_log() +
+  ggplot2::geom_point(aes(degree, survival), size = 0.75) +
+  ggplot2::geom_line(aes(x, S_med), data = df0.fitted, col = 4, lty = 2) +
+  ggplot2::geom_line(aes(x, S_025), data = df0.fitted, col = 2, lty = 3) +
+  ggplot2::geom_line(aes(x, S_975), data = df0.fitted, col = 2, lty = 3)
 gg2
 
 ## -----------------------------------------------------------------------------
-Smix.imports <- Smix(df1.imports$degree, 146, 1.58, 0.68, 30.4, FALSE, 0.0172)
-gg3 <- df1.imports %>%
-    dplyr::mutate(survival.mix = Smix.imports) %>%
-    ggplot2::ggplot() +
-	ggplot2::geom_point(aes(degree, survival), size = 0.75) +
-	ggplot2::geom_line(aes(degree, survival.mix), col = 4, lty = 2, lwd = 1.2) +
-	ggplot2::scale_x_log10() +
-	ggplot2::scale_y_log10() +
-	ggplot2::theme_bw(12)
+gg3 <- df1.imports |>
+  filter(degree > 1L) |>
+  dplyr::mutate(
+    survival = 1.0 - cumsum(frequency) / sum(frequency),
+    survival.mix = Smix2(degree, 1606, 1.73, 1.00, 0.237, 4.03, 0.003)
+  ) |>
+  ggplot2::ggplot() +
+  ggplot2::geom_point(aes(degree, survival), size = 0.75) +
+  ggplot2::geom_line(aes(degree, survival.mix), col = 4, lty = 2, lwd = 1.2) +
+  ggplot2::scale_x_log10() +
+  ggplot2::scale_y_log10() +
+  ggplot2::theme_bw(12)
 gg3
 
